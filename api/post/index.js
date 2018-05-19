de06ecb102994753d887e340a46c8ae974753c84
SeungUkLee <lsy931106@gmail.com>
2018-05-19 23:55:54 +0900

    dev post api

---
+++ api/post/index.js
@@ -0,0 +1,12 @@
+/* Routing */
+const express = require('express');
+const router = express.Router();
+const ctrl = require('./post.ctrl')
+
+router.get('/', ctrl.index)
+router.get('/:id', ctrl.show)
+router.delete('/:id', ctrl.destroy)
+router.post('/', ctrl.create)
+router.put('/:id', ctrl.update)
+
+module.exports = router;
---
+++ api/post/post.ctrl.js
@@ -0,0 +1,25 @@
+/* API logic (controller) */
+
+const index = (req, res) => {
+
+}
+
+const show = (req, res) => {
+
+}
+
+const destroy = (req, res) => {
+
+}
+
+const create = (req, res) => {
+
+}
+
+const update = (req, res) => {
+
+}
+
+module.exports = {
+    index, show, destroy, create, update,
+}
