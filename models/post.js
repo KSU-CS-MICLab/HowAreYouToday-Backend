0183369c799acfd47c51b167e96c286283b41b7c
SeungUkLee <lsy931106@gmail.com>
2018-05-23 21:35:41 +0900

    dev post api - modified post model schema

--- models/post.js
+++ models/post.js
@@ -1,17 +1,46 @@
+const Joi = require('joi')
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-    title: String,
-    content: String,
-    writer: String,
-    created: Date,
-    likes: Number,
-    comments: {
-        name: String,
-        message: String,
-        created: Date
+const Comment = new Schema({
+    createdAt: { type: Date, default: Date.now },
+    username: String, 
+    content: String
+});
+
+const Post = new Schema({
+    createdAt: { type: Date, default: Date.now },
+    writer: { 
+        type: String,
+        required: true,
+    },
+    title: { 
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 100
+    },
+    content: { 
+        type: String,
+        required: true,
+    },
+    likesCount: { type: Number, default: 0 },
+    likes: { type: [String], default: [] },
+    comments: { 
+        type: [Comment],
+        default: []
     }
 });
 
-module.exports = mongoose.model('post', postSchema);
+const validatePost = (post) => {
+    const schema = {
+        title: Joi.string().min(5).max(50).required(),
+        content: Joi.string().min(10).required(),
+        writer: Joi.string().required()
+    }
+}
+
+module.exports = {
+    Post: mongoose.model('post', Post),
+    validate: validatePost
+}
