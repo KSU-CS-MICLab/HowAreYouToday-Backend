2cda6768b5b4cd90db727427f28d9041009769f5
SeungUkLee <lsy931106@gmail.com>
2018-05-23 18:21:55 +0900

    dev post api - use async/await

--- api/post/post.ctrl.js
+++ api/post/post.ctrl.js
@@ -1,56 +1,48 @@
 /* API logic (controller) */
-const Post = require('../../models/posts')
+const Post = require('../../models/post')
 
-const index = (req, res) => {
-    Post.find(function(err, posts){
-        if(err) return res.status(500).send({error: 'database failure'});
-        res.json(posts);
-    })
+const index = async (req, res) => {
+    const posts = await Post.find()
+
+    res.send(posts)
 }
 
-const show = (req, res) => {
-    Post.findOne({_id: req.params.id}, function(err, post){
-        if(err) return res.status(500).json({error: err});
-        if(!post) return res.status(404).json({error: 'post not found'});
-        res.json(post);
-    })
+const show = async (req, res) => {
+    const post = await Post.findById(req.params.id)
+
+    if (!post) return post.status(404).send('The post was not found. :)')
+
+    res.send(post)    
 }
 
-const destroy = (req, res) => {
-    Post.remove({ _id: req.params.id }, function(err, output){
-        if(err) return res.status(500).json({ error: "database failure" });
+const destroy = async (req, res) => {
+    const post = await Post.findByIdAndRemove(req.params.id)
 
-        // ( SINCE DELETE OPERATION IS IDEMPOTENT, NO NEED TO SPECIFY )
-        if(!output.result.n) return res.status(404).json({ error: "post not found" });
-        res.json({ message: "post deleted" });
-        
-        res.status(204).end();
-    })
+    if (!post) return post.status(404).send('The post was not found. :)')
+
+    res.send(post)
 }
 
-const create = (req, res) => {
-    const post = new Post();
-    post.title = req.body.title;
-    post.content = req.body.content;
+const create = async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    })
 
-    post.save(function (err) {
-        if (err) {
-            console.error(err);
-            res.json({ result: 0 });
-            return;
-        }
-        res.json({ result: 1 });
+    await post.save()
 
-    });
+    res.send(post)
 }
 
-const update = (req, res) => {
-    Post.update({ _id: req.params.id }, { $set: req.body }, function(err, output){
-        if(err) res.status(500).json({ error: 'database failure' });
-        console.log(output);
-        if(!output.n) return res.status(404).json({ error: 'post not found' });
-        res.json( { message: 'post updated' } );
+const update = async (req, res) => {
+    const post = await Post.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        content: req.body.content
     })
+
+    if (!post) return post.status(404).send('The post was not found. :)')
+    
+    res.send(post)
 }
 
 module.exports = {
