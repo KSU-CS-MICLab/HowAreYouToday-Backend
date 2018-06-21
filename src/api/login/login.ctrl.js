9202447915dee278fada1d9d2c1baf0a2e9f4d40
SeungUkLee <lsy931106@gmail.com>
2018-06-22 07:19:29 +0900

    test login

--- src/api/login/login.ctrl.js
+++ src/api/login/login.ctrl.js
@@ -4,15 +4,15 @@ const asyncMiddleware = require('../../middleware/async')
 const login = asyncMiddleware (async (req, res) => {
     const user = await Account.find().where('uid').equals(req.user.uid)
 
+    const account = new Account({
+        uid: req.user.uid,
+        email: req.user.email
+    })
     if (!user) {
-        const account = new Account({
-            uid: req.user.uid,
-            email: req.user.email
-        })
         await account.save()
     }
 
-    res.send({'data':'success'})
+    res.send(account)
 })
 
 module.exports = {
