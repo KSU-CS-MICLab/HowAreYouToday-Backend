e910c180e6457a91802b75fe6b7b478156d277e1
SeungUkLee <lsy931106@gmail.com>
2018-06-22 07:40:12 +0900

    fix bug - login

--- src/api/login/login.ctrl.js
+++ src/api/login/login.ctrl.js
@@ -3,16 +3,17 @@ const asyncMiddleware = require('../../middleware/async')
 
 const login = asyncMiddleware (async (req, res) => {
     const user = await Account.find().where('uid').equals(req.user.uid)
-
-    const account = new Account({
-        uid: req.user.uid,
-        email: req.user.email
-    })
-    if (!user) {
+    
+    // TODO: 빈 객체 체크 다른방법은?
+    if (Object.keys(user).length === 0 ) {
+        const account = new Account({
+            uid: req.user.uid,
+            email: req.user.email
+        })
         await account.save()
     }
 
-    res.send(account)
+    res.send({'data':'success'})
 })
 
 module.exports = {
