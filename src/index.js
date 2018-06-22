5c74f606a0c7e3fca80cb979ad4d31baa12088b5
SeungUkLee <lsy931106@gmail.com>
2018-06-22 11:18:09 +0900

    modified index.file

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const firebase = require("firebase-admin");
 
 app.use(express.json());
 app.use(cors())
-app.use('/posts', firebaseAuth, post);
+app.use('/posts',  firebaseAuth, post);
 app.use('/comments', firebaseAuth, comment);
 app.use('/likes', firebaseAuth, like);
 app.use('/login', firebaseAuth, login);
@@ -24,16 +24,14 @@ const port = process.env.PORT || 3030;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 const db = mongoose.connection
-// mongoose.connect(`mongodb://${process.env.DB_HOST || 'localhost'}:27017/${process.env.DB}`)
-mongoose.connect(`mongodb://localhost:27017/test1`)
+mongoose.connect(`mongodb://${process.env.DB_HOST || 'localhost'}:27017/${process.env.DB}`)
 db.on('error', console.error)
 db.once('open', function(){
     // CONNECTED TO MONGODB SERVER
     console.log("Connected to mongod server")
 })
 
-// const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY)
-const serviceAccount = require('../serviceAccountKey.json')
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY)
 firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount)
 });
